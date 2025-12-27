965b5446f823768529809be44d0f307ac15da432
Shin Seungcheol <ani7981@gmail.com>
2025-12-27 16:10:47 +0900

    스프링 시큐리티 보안설정 변경

--- src/main/java/com/doggo/doggo/controller/QnaController.java
+++ src/main/java/com/doggo/doggo/controller/QnaController.java
@@ -7,12 +7,14 @@ import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class QnaController {
 
     @Autowired
--- src/main/java/com/doggo/doggo/controller/TrackingController.java
+++ src/main/java/com/doggo/doggo/controller/TrackingController.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RequestMapping("/api/tracking")
 public class TrackingController {
